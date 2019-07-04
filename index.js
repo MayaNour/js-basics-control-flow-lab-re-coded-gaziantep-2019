6507193e376b50345c19900898398ee6d36ffdf2
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-04 19:49:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function scuberGreetingForFeet(feetValue){
   // Write your code here!
-  if(feetValue >= 2500)
+  if(feetValue > 2500)
+    return 'No can do.';
+  else if(feetValue > 2000)
+    
 }
 
 function ternaryCheckCity(){
